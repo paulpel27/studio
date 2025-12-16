035e87103bff41e6d89fb10bba77571610478fad
Paul Pel <pelpau@gmail.com>
2025-12-16 13:42:35 +0000

    Body exceeded 1 MB limit. To configure the body size limit for Server Ac

--- src/app/api/genkit/[[...flow]]/route.ts
+++ src/app/api/genkit/[[...flow]]/route.ts
@@ -9,9 +9,9 @@ import '@/ai/flows/extract-text-from-pdf-flow';
 export const POST = createNextHandler();
 
 export const config = {
-    api: {
-        bodyParser: {
-            sizeLimit: '15mb',
-        },
+  api: {
+    bodyParser: {
+      sizeLimit: '15mb',
     },
+  },
 };
